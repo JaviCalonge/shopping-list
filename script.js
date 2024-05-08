421850fac7d2ed36efa8be6085796e9e4edb70a2
JaviCalonge <javicalongem@gmail.com>
2024-05-08 10:41:06 +0200

    Romove Items from Local Storage

--- script.js
+++ script.js
@@ -81,20 +81,41 @@ function getItemsFromStorage () {
   return itemsFromStorage
 }
 
-function removeItem (e) {
+function onClickItem (e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
-    if (confirm("Are you sure?")) {
-      e.target.parentElement.parentElement.remove()
-
-      checkUI()
-    }
+    removeItem(e.target.parentElement.parentElement)
   }
+}
+
+function removeItem (item) {
+  if (confirm("Are you sure?")) {
+    //Remove item from DOM
+    item.remove()
+
+    //Remove item from Storage
+    removeItemFromStorage (item.textContent) 
+
+    checkUI()
+  }
+}
+
+function removeItemFromStorage (item) {
+  let itemsFromStorage = getItemsFromStorage()
+
+  //Filter out item to be remove
+  itemsFromStorage = itemsFromStorage.filter((i) => i !== item)
+
+  //Re-set to Local Storage
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage))
 }
 
 function clearItems () {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild)
   }
+  //Clear from Local Storage
+  localStorage.removeItem("items")
+
   checkUI()
 }
 
@@ -131,7 +152,7 @@ function checkUI () {
 function init () {
 //Event Listeners
 itemForm.addEventListener("submit", onAddItemSubmit)
-itemList.addEventListener("click", removeItem)
+itemList.addEventListener("click", onClickItem)
 clearBtn.addEventListener("click", clearItems)
 itemFilter.addEventListener("input", filterItems)
 document.addEventListener("DOMContentLoaded", displayItems)
